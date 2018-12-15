6cdd602abc79c557e95105b1e8d46dc153b43b38
Will Schoenberger <will.schoenberger@hyfn.com>
2018-12-14 22:41:08 -0800

    Removed example stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,103 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react'
 import Influencers from 'components/Influencers'
 import InfluencerItem from 'components/InfluencerItem'
 import InfluencerDetails from 'components/InfluencerDetails'
-import logo from './logo.svg';
 
-import './App.css';
+export const App = () => (
+  <div className='App'>
+    {/* <InfluencerItem id={1601235883} /> */}
+    <InfluencerDetails id={1601235883} />
+    {/* <Influencers /> */}
+  </div>
+)
 
-const bodyString = `
-query Influencers {
-  influencers {
-    id
-    handle
-    avatar
-    twitchId
-    twitchFollowers
-    twitchViewers
-    twitchUrl
-    twitterFollowers
-    twitterLink
-    youtubeSubscribers
-    youtubeLink
-  }
-}
-`
-
-class App extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('http://localhost:4000/graphql', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        query: bodyString
-      }),
-    });
-    const body = await response.text();
-
-    this.setState({ responseToPost: body });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
-        {/* <InfluencerItem id={1601235883} /> */}
-        <InfluencerDetails id={1601235883} />
-        {/* <Influencers /> */}
-      </div>
-    );
-  }
-}
-
-export default App;
+export default App
